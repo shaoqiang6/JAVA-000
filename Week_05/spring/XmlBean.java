48684250ea395e557834d775b4a1c46a89af875f
yanshaoqiang <shaoqiang_yan@163.com>
2020-11-18 00:15:29 +0800

    load spring bean

---
+++ Week_05/spring/XmlBean.java
@@ -0,0 +1,13 @@
+package com.shawn.spring;
+
+/**
+ * @author yansq
+ * @date 2020/11/17
+ */
+public class XmlBean {
+
+    public XmlBean() {
+        System.out.println("xmlBean init ");
+    }
+
+}
