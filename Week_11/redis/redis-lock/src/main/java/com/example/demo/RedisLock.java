5fcb0a161e67c0104593b1218380af3ba07d20d0
yanshaoqiang <shaoqiang_yan@163.com>
2021-01-06 20:03:14 +0800

    redis lock

---
+++ Week_11/redis/redis-lock/src/main/java/com/example/demo/RedisLock.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import org.redisson.api.RBucket;
+import org.redisson.api.RScript;
+import org.redisson.api.RedissonClient;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yansq
+ * @date 2021/1/5
+ */
+
+public class RedisLock {
+
+
+    public static boolean lock(String lockName, long timeOut, TimeUnit timeUnit) {
+        RedissonClient client = RedissonClientConfig.getClient();
+        RBucket<String> bucket = client.getBucket(lockName);
+        return bucket.trySet("1", timeOut, timeUnit);
+    }
+
+    public static boolean release(String lockName) {
+        String script = "if redis.call('exists',KEYS[1]) == 1 then " +
+                "return redis.call('del',KEYS[1]) else return 0 end";
+        RedissonClient client = RedissonClientConfig.getClient();
+        Long eval = client.getScript().eval(RScript.Mode.READ_WRITE,
+                script,
+                RScript.ReturnType.INTEGER,
+                Collections.singletonList(lockName));
+        System.out.println("eval--=====================" +eval);
+        return Objects.equals(eval,1L);
+    }
+
+    public static int scriptTest(String key) {
+        String s = "return redis.call('get',KEYS[1])";
+        RedissonClient client = RedissonClientConfig.getClient();
+        Object eval = client.getScript().eval(RScript.Mode.READ_ONLY,
+                s,
+                RScript.ReturnType.INTEGER,
+                Collections.singletonList(key));
+//        boolean Integ = eval instanceof Integer;
+//        System.out.println(Integ);
+        return 1;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String lockName = "cui:lock1";
+        boolean cuiLock = lock(lockName, 300L, TimeUnit.SECONDS);
+        System.out.println("=================== cuiLock" + cuiLock);
+        TimeUnit.SECONDS.sleep(1L);
+        boolean release = release(lockName);
+        System.out.println("================= release" + release);
+        boolean exists = RedissonClientConfig.getClient().getBucket(lockName).isExists();
+        System.out.println(exists);
+        int i = scriptTest(lockName);
+        System.out.println(i);
+        RedissonClientConfig.shutdown();
+    }
+
+
+}
---
+++ Week_11/redis/redis-lock/src/main/java/com/example/demo/RedissonClientConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.config.Config;
+
+/**
+ * @author yansq
+ * @date 2021/1/5
+ */
+public class RedissonClientConfig {
+
+    private RedissonClientConfig() {
+    }
+
+    public static RedissonClient getClient() {
+        return RedissonConfigHolder.REDISSON_CLIENT;
+    }
+
+    public static void shutdown() {
+        RedissonConfigHolder.REDISSON_CLIENT.shutdown();
+    }
+
+    private static class RedissonConfigHolder {
+        private static final RedissonClient REDISSON_CLIENT = getClient();
+
+        private static RedissonClient getClient() {
+            Config config = new Config();
+            config.setCodec(new StringCodec());
+            config.useSentinelServers().addSentinelAddress(
+                    "redis://192.168.252.40:26379",
+                    "redis://192.168.252.40:26380",
+                    "redis://192.168.252.40:26381"
+            ).setMasterName("promotion_center");
+            return Redisson.create(config);
+        }
+    }
+}
